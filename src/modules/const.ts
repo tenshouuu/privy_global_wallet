1a42af043559bff525605548b7c9b3ff27dbc7b9
tenshou <peter.m@ankrlabs.xyz>
2025-10-08 16:03:19 +0700

    feat: add text

--- src/modules/const.ts
+++ src/modules/const.ts
@@ -1,6 +1,7 @@
 
 export const DEFAULT_CHAIN_ID = 267; // NeuraTestnet - In the future 266 NeuraMainnet
 
+// Replace with your Privy App ID
 export const PRIVY_APP_ID = 'cmfy2z7jy0066jr0d82plck5z';
 
 export const NEURA_PROVIDER_APP_ID = 'cmbpempz2011ll10l7iucga14';
